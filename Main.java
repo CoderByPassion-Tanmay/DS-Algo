c109531bc62350195c25654b7bf651bf91097317
coderbypassion <>
2023-04-05 12:09:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+ // Span of Array  
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+  Scanner sc = new Scanner(System.in);
+int a =  sc.nextInt();
+ int arr[] = new int[a];
+
+  
+    
+}
+
+}

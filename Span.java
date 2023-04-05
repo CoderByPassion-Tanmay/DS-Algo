268ed88d0b3019de09748cfcebcb6ac0f27e50aa
Tanmay <109308432+CoderByPassion-Tanmay@users.noreply.github.com>
2023-04-05 12:40:17 +0000

    55

--- Span.java
+++ Span.java
@@ -7,15 +7,15 @@ public class Span{
   public static void main(String[] args) throws Exception {
     // write your code here
     System.out.println("terrrr");
-    // Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-    // int a = sc.nextInt();
-    // int arr[] = new int[a];
+    int a = sc.nextInt();
+    int arr[] = new int[a];
 
-    // for(int i=0 ;i<arr.length;i++)
-    // {
-    // arr[i] = sc.nextInt();
-    // }
+    for(int i=0 ;i<arr.length;i++)
+    {
+    arr[i] = sc.nextInt();
+    }
 
   }
 

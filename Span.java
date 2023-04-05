ebb8a63e519cd8435f2210fcb8f8fa249a4e4ded
Tanmay <109308432+CoderByPassion-Tanmay@users.noreply.github.com>
2023-04-05 13:00:08 +0000

    SPan of array

--- Span.java
+++ Span.java
@@ -16,7 +16,22 @@ public class Span{
     {
     arr[i] = sc.nextInt();
     }
+        int max = findMax(arr);
+     System.out.println("MAx ="+max);
 
+       
   }
+  public static int findMax(int arr[]){
+    int max = 0;
+   for(int i=0 ;i<arr.length;i++)
+    {
+    if (arr[i] > max)
+    {
+      max = arr[i];
+    }
+      
+    }
 
+    return max;
+  }
 }
